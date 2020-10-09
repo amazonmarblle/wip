c0144be60ddb90b167f6869cd76b8b9849b1f707
Abhishek Jain <amazonmarblle@gmail.com>
2020-10-10 00:07:45 +0530

    Enahnced slider time for product detail page

--- src/app/product/components/product-detail-page/product-images/product-images.component.ts
+++ src/app/product/components/product-detail-page/product-images/product-images.component.ts
@@ -33,7 +33,7 @@ export class ProductImagesComponent implements OnInit {
       grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
       slide: 1,
       speed: 400,
-      interval: {timing: 3000, initialDelay: 1000},
+      interval: {timing: 1000, initialDelay: 500},
       point: {
         visible: true,
         hideOnSingleSlide: true // To apply our modification
